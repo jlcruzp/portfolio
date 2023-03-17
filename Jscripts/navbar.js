32b93f57c0682abea544b93bad1f589bc91e43cb
juan <jcruzpea@hotmail.com>
2023-03-17 13:01:57 -0600

    Primer commit pagina funcional hasta experiencia en programacion

---
+++ Jscripts/navbar.js
@@ -0,0 +1,11 @@
+const header = document.querySelector(".page-header");
+const toggleClass = "is-sticky";
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 150) {
+    header.classList.add(toggleClass);
+  } else {
+    header.classList.remove(toggleClass);
+  }
+});
